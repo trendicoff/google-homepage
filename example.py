5eacc1f384539d7630ce91e1e60f3089e00a9924
Ari <trendicoff@gmail.com>
2022-06-21 15:04:19 -0400

    added win condition

--- example.py
+++ example.py
@@ -1,16 +1,22 @@
 #Step 1 
 import random
+from re import A
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
-
+stage = []
+for x in chosen_word:
+    stage += "_"
 
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("What letter?").lower()
-#print (guess)
+while "_" in stage:
+    guess = input("What letter? ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-for x in word_list:
-  print(x)
+    for x in range(len(chosen_word)):
+        letter = chosen_word[x]
+        if letter == guess:
+            stage[x] = guess
+    print(stage,"\n")
+print (f"You won! the word was {chosen_word}")
